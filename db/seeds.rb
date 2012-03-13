a20fed864f191a9fe081a8a68a96cb80e9fd7d25
Oscar Ramos <oscarramosp@gmail.com>
2012-03-12 21:28:13 -0500

    Commit de la clase

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Zombie.create(name:'Oscar', graveyard:'UPC')
+Zombie.create(name:'Blitz', graveyard:'GPC')
+Tweet.create(status:'Hola mundo', zombie_id: '1')
+Tweet.create(status:'Hola mundo 2222', zombie_id: '1')
+Tweet.create(status:'Hola mundo', zombie_id: '2')
+Tweet.create(status:'Soy trolo', zombie_id: '2')
